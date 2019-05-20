5494a7dbf7d25cde600b7bccf814f52bda28a18a
hiroys <46834972+hiroys@users.noreply.github.com>
2019-05-20 16:14:07 +0900

    デコードしなくていい場所をデコードしていた不具合を修正

--- twibot.py
+++ twibot.py
@@ -31,7 +31,7 @@ def get_my_tweet():
         while len(tw_str.encode('utf-8')) > 132:
             tw_str = tw_str[:-1]
         tw = {
-            'tweet_word': tw_str.decode('utf-8') + '(' + str(n_hour) + ')'
+            'tweet_word': tw_str + '(' + str(n_hour) + ')'
         }
         tweets.append(tw)
 
